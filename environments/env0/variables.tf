f1cdf38b9af2b793d39f6adc7e4e9adfe0e83bee
rasj81 <76783231+rasj81@users.noreply.github.com>
2025-03-19 18:00:57 -0400

    Update variables.tf

--- environments/env0/variables.tf
+++ environments/env0/variables.tf
@@ -1,6 +1,6 @@
 variable "credentials" {
   description = "My Credentials"
-  default     = "../.creds/my-credentials.json"
+  default     = "../gcp-credentials.json"
   #ex: if you have a directory where this file is called keys with your service account json file
   #saved there as my-creds.json you could use default = "./keys/my-creds.json"
 }
